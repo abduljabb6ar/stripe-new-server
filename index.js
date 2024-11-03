1675d39ff7a88022a34abcaa09d9f0fa2e50d9ce
abduljabbar <abduljab6ar@gmail.com>
2024-11-03 20:59:47 +0300

    اضافة الايميل والباس عند الانقال الى صفحة نجاح

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const stripe=require('stripe')(process.env.STRIPE_SECRIT)
 const port=8000||process.env.PORT;
 var app=express();
-
+const crypto = require('crypto');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -45,6 +45,16 @@ app.get('/',(req,res)=>{
 //     res.status(400).json({ error: error.message });
 //   }
 // });
+function encrypt(text, key) {
+  const iv = crypto.randomBytes(16); // الموجه الأولي (IV) عشوائي
+  const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  // دمج IV مع النص المشفر
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
+}
+const encryptionKey = 'abduljabbarfuadamer1372001abdulj'; // تأكد أن المفتاح طوله 32 حرفًا
 app.post('/checkout', async (req, res) => {
   try {
       const price = parseInt(req.body.price, 10);
@@ -52,8 +62,12 @@ app.post('/checkout', async (req, res) => {
       const userId = req.body.userId; // معرف المستخدم من Firebase
       const userEmail = req.body.userEmail; // البريد الإلكتروني
       const shippingAddress = req.body.shippingAddress; // عنوان الشحن
-      const token = req.body.token; // عنوان الشحن
-      const successUrl = `https://ghidhaalruwhusa.com/success?token=${token}`;
+      // const token = req.body.token; // عنوان الشحن
+      const password = req.body.token; // عنوان الشحن
+
+const encryptedEmail = encrypt(userEmail, encryptionKey);
+const encryptedPassword = encrypt(password, encryptionKey);
+      const successUrl = `https://ghidhaalruwhusa.com/success?email=${encodeURIComponent(encryptedEmail)}&password=${encodeURIComponent(encryptedPassword)}`;
       // // const line2 = req.body.line2; // عنوان الشحن
       // const city = req.body.city; // عنوان الشحن
       // const state = req.body.state; // عنوان الشحن
